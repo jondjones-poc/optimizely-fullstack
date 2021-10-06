eea705d4ae43cc1c701e576cf137621b6dac9bab
Jon <jon.jones.home@gmail.com>
2021-10-06 21:19:00 +0100

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using OptimizelySDK;
+using System;
+using System.Web.Mvc;
+
+namespace OptimizelyFullstackSampleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var optimizelyInstance = OptimizelyFactory.NewDefaultInstance("KcJRoKbnxrcrRa5QGUSjy");
+
+            var userId = Guid.NewGuid().ToString();
+            var featureEnabled = optimizelyInstance.IsFeatureEnabled("discount", userId);
+
+            if (featureEnabled)
+            {
+                var discountAmount = optimizelyInstance.GetFeatureVariableInteger("discount", "amount", userId);
+                optimizelyInstance.Track("purchase", userId);
+                throw new Exception($"feature flag enabled discount variable = {discountAmount}");
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptimizelyFullstackSampleSite.MvcApplication" Language="C#" %>
